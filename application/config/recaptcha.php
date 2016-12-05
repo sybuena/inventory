50bb54f49ad8308486bbcd40b63be7fc802b7934
Sy <sybuena@gmail.com>
2016-12-05 15:25:01 -0800

    recaptcha

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,4 +1,14 @@
 <?php //-->
 
-$config['recaptcha_site_key'] = '6LcJbiUTAAAAAFOEepBAtGQKCzSDBKuFAbIIHZyT';
-$config['recaptcha_secret_key'] = '6LcJbiUTAAAAANaa5xNkrwCk1wFRLZosDqNgeTgB';
+if($_SERVER['SERVER_NAME'] == 'dev.apgars-inventory.com') {
+	$config['recaptcha_site_key'] = '6Lfi-A0UAAAAAPkSXDtXQbAXgMEpI0wlrGEVgFbH';
+	$config['recaptcha_secret_key'] = '6Lfi-A0UAAAAAAT6IsEMh_YlHR4xT7R-qerueYga';
+//for LIVE SERVER/DATABASE
+} else if($_SERVER['SERVER_NAME'] == 'apgars-inventory.com') {
+	$config['recaptcha_site_key'] = '6Lfi-A0UAAAAAPkSXDtXQbAXgMEpI0wlrGEVgFbH';
+	$config['recaptcha_secret_key'] = '6Lfi-A0UAAAAAAT6IsEMh_YlHR4xT7R-qerueYga';
+//else DEFAULT LOCAL
+} else {
+	$config['recaptcha_site_key'] = '6LeT-A0UAAAAABEiyyPM1s1oMN1nnqZPEP8qlFsz';
+	$config['recaptcha_secret_key'] = '6LeT-A0UAAAAAFrLOoFlCGwGpVjtAa5GKJGkujzl';
+}
