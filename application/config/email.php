fc9afee26b2099ae26afec5dcf7a5120d78fc6ba
sybuenavista <sybuena@gmail.com>
2019-10-11 16:38:56 +0800

    update

--- application/config/email.php
+++ application/config/email.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-include dirname(__FILE__).'/../../config.php';
+//include dirname(__FILE__).'/../../config.php';
 
 $config['mailtype']           = '';
 $config['charset']            = '';
